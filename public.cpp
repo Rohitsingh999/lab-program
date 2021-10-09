21ebd3d53585b0dbe1a42f75253ea58bb19e583d
Rohitsingh999 <92010674+Rohitsingh999@users.noreply.github.com>
2021-10-09 11:47:15 +0530

    Add files via upload

---
+++ public.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class base{
+public :
+int x;
+int y;
+};
+
+int main(){
+ base r1,r2;
+  
+ r1.x=10;
+ r1.y=20;
+ r2.x=30;
+ r2.y =50;
+ 
+
+ cout<<r1.x<<endl;
+
+ cout<<r1.y<<endl;
+
+ cout<<r2.x<<endl;
+
+ cout<<r2.x<<endl;
+
+
+return 0;
+}
